0925958d89ce8f078d3352fb05c9d2f8841b62c8
Jonathan Euzebe <60105810+Jo152@users.noreply.github.com>
2021-12-06 12:58:27 -0500

    Update favorite.blade.php

--- resources/views/favorite.blade.php
+++ resources/views/favorite.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Favorite') }}
@@ -15,10 +17,11 @@
 {{--                @endforeach--}}
             </div>
             <div>
-                <a href="/favorite/{{$favorite->id}}/delete"> delete from favorites</a>
+                <a href="/favorite/{{$favorite->id}}/delete"> <i class="far fa-heart"></i></a>
             </div>
         </article>
 
-    <a href="/favorites">Go back</a>
+    <a href="/favorites"><i class="fas fa-arrow-circle-left"></i></a>
     </body>
+   
 </x-app-layout>
